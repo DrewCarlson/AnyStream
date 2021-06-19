03199460fc04145eab2ef2d43cc47f36ee04fabb
Drew Carlson <awcarlson@protonmail.com>
2021-06-19 15:58:41 -0700

    Remove some old web files

--- client-web-old/webpack.config.d/css.js
+++
@@ -1,1 +0,0 @@
-config.module.rules.push({ test: /\.css$/, use:["style-loader","css-loader"] });
--- client-web-old/webpack.config.d/webpack.js
+++
@@ -1,7 +0,0 @@
-config.resolve.modules.push("../../processedResources/js/main");
-
-if (config.devServer) {
-    config.devServer.historyApiFallback = true;
-    config.devServer.hot = true;
-    config.output.publicPath = '/';
-}
