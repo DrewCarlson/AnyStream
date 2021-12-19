caa29a57c474c59a6384d06dbe185bf1647db114
Drew Carlson <awcarlson@protonmail.com>
2021-12-18 22:30:22 -0800

    Fix web icon webpack configuration

--- anystream-client-web/webpack.config.d/bootstrap-icons.js
+++ anystream-client-web/webpack.config.d/bootstrap-icons.js
@@ -1,1 +1,7 @@
-config.module.rules.push({test: /\.woff(2)?(\?\S+)?$/, loader: 'file-loader'});
+config.module.rules.push({
+    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    type: "asset/resource",
+    generator: {
+        filename: "fonts/[name][ext]"
+    }
+});
