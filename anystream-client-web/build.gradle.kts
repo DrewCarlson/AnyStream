9917a127978908da6b52df9262848c66e9d72e5e
Drew Carlson <awcarlson@protonmail.com>
2021-12-18 22:30:03 -0800

    Remove kotlin/webpack patch

--- anystream-client-web/build.gradle.kts
+++ anystream-client-web/build.gradle.kts
@@ -62,9 +62,3 @@ kotlin {
         }
     }
 }
-
-// Override webpack version to fix dev-server error: https://youtrack.jetbrains.com/issue/KT-49124
-rootProject.extensions
-    .configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
-        versions.webpackCli.version = "4.9.1"
-    }
