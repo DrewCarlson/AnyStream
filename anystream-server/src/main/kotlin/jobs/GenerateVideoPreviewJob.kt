f63bab36058e6069f7139873c2b0f30e6b0d57a4
Drew Carlson <awcarlson@protonmail.com>
2022-03-24 13:28:03 -0700

    [Server] Disable preview generation

--- anystream-server/src/main/kotlin/jobs/GenerateVideoPreviewJob.kt
+++ anystream-server/src/main/kotlin/jobs/GenerateVideoPreviewJob.kt
@@ -49,7 +49,7 @@ object GenerateVideoPreviewJob : Job("generate-video-preview") {
         mediaDbQueries: MediaDbQueries,
     ) {
         val previewStorage = "${rootStorageDir}${File.separator}previews"
-        kjob.register(GenerateVideoPreviewJob) {
+        /*kjob.register(GenerateVideoPreviewJob) {
             execute {
                 val mediaRefId = props[MEDIA_REF_ID]
                 when (val mediaRef = mediaDbQueries.findMediaRefById(mediaRefId)) {
@@ -59,14 +59,14 @@ object GenerateVideoPreviewJob : Job("generate-video-preview") {
                     is LocalMediaReference -> generatePreview(logger, ffmpeg(), previewStorage, mediaRef)
                 }
             }
-        }
+        }*/
     }
 
     suspend fun schedule(kjob: KJob, mediaRefId: String) {
-        kjob.schedule(GenerateVideoPreviewJob) {
+        /*kjob.schedule(GenerateVideoPreviewJob) {
             jobId = "$name-$mediaRefId"
             props[MEDIA_REF_ID] = mediaRefId
-        }
+        }*/
     }
 
     private suspend fun generatePreview(
