1748c72bbea4d4424f02137985daafa0585bc0a6
Drew Carlson <awcarlson@protonmail.com>
2023-04-20 18:53:13 -0700

    Set java versions in sql-generator

--- libs/sql-generator/build.gradle.kts
+++ libs/sql-generator/build.gradle.kts
@@ -7,3 +7,18 @@ dependencies {
     implementation(libs.ksp)
     implementation(projects.libs.sqlGeneratorApi)
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    target {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+}
