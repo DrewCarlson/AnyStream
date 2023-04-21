d92e6ff247b9398e6ddd4ce75d77929802a3eac2
Drew Carlson <awcarlson@protonmail.com>
2023-04-20 18:33:14 -0700

    Specify java version for sql-generator-api

--- libs/sql-generator-api/build.gradle.kts
+++ libs/sql-generator-api/build.gradle.kts
@@ -1,3 +1,18 @@
 plugins {
     kotlin("jvm")
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    target {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+}
