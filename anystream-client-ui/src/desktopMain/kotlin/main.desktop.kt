9daca7d6a7c23f12b5e8e221b96eb20d1f44c0cc
amitbeans <amits@beans.com>
2023-07-01 18:11:06 -0400

    Create Desktop client

---
+++ anystream-client-ui/src/desktopMain/kotlin/main.desktop.kt
@@ -0,0 +1,12 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import anystream.ui.App
+
+@Composable
+fun MainView() = App()
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
+}
