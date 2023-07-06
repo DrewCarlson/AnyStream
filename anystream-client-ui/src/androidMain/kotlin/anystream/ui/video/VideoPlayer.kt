9ca1164127c749c0df3f7214b1923ca54ca309e2
Drew Carlson <awcarlson+git@protonmail.com>
2023-07-06 00:39:42 -0700

    Implement iOS video playback

--- anystream-client-ui/src/androidMain/kotlin/anystream/ui/video/VideoPlayer.kt
+++ anystream-client-ui/src/androidMain/kotlin/anystream/ui/video/VideoPlayer.kt
@@ -1,3 +1,20 @@
+/**
+ * AnyStream
+ * Copyright (C) 2023 AnyStream Maintainers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package anystream.ui.video
 
 import android.view.ViewGroup
@@ -13,11 +30,11 @@ import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
 @Composable
-internal actual fun VideoPlayer(modifier: Modifier, url: String) {
+internal actual fun VideoPlayer(modifier: Modifier, mediaLinkId: String) {
     val context = LocalContext.current
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
-            val mediaItem = MediaItem.fromUri(url)
+            val mediaItem = MediaItem.fromUri(mediaLinkId)
             setMediaItem(mediaItem)
             prepare()
             playWhenReady = true
