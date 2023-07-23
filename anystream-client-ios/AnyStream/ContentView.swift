eba0bec641548614fb4e53bd542caf809f0abc34
Drew Carlson <awcarlson+git@protonmail.com>
2023-07-22 21:22:50 -0700

    Remove old swift code

--- anystream-client-ios/AnyStream/ContentView.swift
+++ anystream-client-ios/AnyStream/ContentView.swift
@@ -1,24 +1,6 @@
 import AnyStreamCore
 import SwiftUI
 
-struct ContentView: View {
-    @StateObject var router: Router = Router()
-    
-    var body: some View {
-        return AnyView(ZStack(alignment: .center) {
-            displayRoute(route: router.stack.last!)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }.frame(maxWidth: .infinity, maxHeight: .infinity))
-    }
-    
-    @ViewBuilder func displayRoute(route: Routes) -> some View {
-        switch route {
-        case _ as Routes.Login:
-            LoginScreen(router: router, anystreamClient: DependencyGraphKt.getClient())
-        default: Text("unknown route")
-        }
-    }
-}
 
 let gradient = LinearGradient(
     colors: [
